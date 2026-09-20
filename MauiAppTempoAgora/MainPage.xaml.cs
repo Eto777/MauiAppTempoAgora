8f50f5aa020cb50c75b69bab021c9d9bef3f7f63
Eto777 <xplagot@gmail.com>
2026-09-20 20:44:24 -0300

    Implementa Agenda 7

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velocidade do Vento: {t.speed} m/s \n" +
+                                         $"Visibilidade: {t.visibility} m \n";
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            }
+            catch (HttpRequestException)
+            {
+                await DisplayAlert(
+                    "Sem conexão",
+                    "Verifique sua conexão com a internet e tente novamente.",
+                    "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
